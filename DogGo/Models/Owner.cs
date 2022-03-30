cf088a74c5d8a16065c3a240c0da0c4a29dc5651
jaypetty <jayshaun2343@gmail.com>
2022-03-30 15:55:46 -0500

    refactored owners

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -11,26 +11,12 @@ namespace DogGo.Models
     {
         public int Id { get; set; }
 
-        [EmailAddress]
-        [Required]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
-        [MaxLength(35)]
         public string Name { get; set; }
-
-        [Required]
-        [StringLength(55, MinimumLength = 5)]
         public string Address { get; set; }
-
-        [Required]
-        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
-
-        [Required]
-        [Phone]
         public string Phone { get; set; }
-        public Neighborhood Neighborhood { get; set; }
         public List<Dog> Dogs { get; set; }
+        public Neighborhood Neighborhood { get; set; }
     }
 }
